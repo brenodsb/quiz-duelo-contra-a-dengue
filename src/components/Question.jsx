1964caf0e4bfe60927d379fe440f58673393251e
Breno Sousa <brenosousabraga@gmail.com>
2024-06-14 19:44:57 -0300

    Adiciona responsividade

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -37,24 +37,26 @@ const Question = () => {
           />
         ))}
       </div>
-      {!quizState.answerSelected && !quizState.help && (
-        <>
-          {currentQuestion.tip && (
-            <button onClick={() => dispatch({ type: "SHOW_TIP" })}>Dica</button>
-          )}
-          <button onClick={() => dispatch({ type: "REMOVE_OPTION" })}>
-            Excluir uma
+      <div id="tips">
+        {!quizState.answerSelected && !quizState.help && (
+          <>
+            {currentQuestion.tip && (
+              <button onClick={() => dispatch({ type: "SHOW_TIP" })}>Dica</button>
+            )}
+            <button onClick={() => dispatch({ type: "REMOVE_OPTION" })}>
+              Excluir uma
+            </button>
+          </>
+        )}
+        {!quizState.answerSelected && quizState.help === "tip" && (
+          <p>{currentQuestion.tip}</p>
+        )}
+        {quizState.answerSelected && (
+          <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>
+            Continuar
           </button>
-        </>
-      )}
-      {!quizState.answerSelected && quizState.help === "tip" && (
-        <p>{currentQuestion.tip}</p>
-      )}
-      {quizState.answerSelected && (
-        <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>
-          Continuar
-        </button>
-      )}
+        )}
+      </div>
     </div>
   );
 };
