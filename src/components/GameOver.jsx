c6f5e5b915731ddf1e8b7d4ac0aa317478c5d048
Tamires <tammymarques116@gmail.com>
2024-06-21 11:36:16 -0300

    Restaurando versão

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -9,10 +9,21 @@ import "./GameOver.css";
 const GameOver = () => {
   const [quizState, dispatch] = useContext(QuizContext);
 
+  const outputEndGame = () => {
+    const score = quizState.score;
+    if(score <= 3) {
+      return 'Que pena! Você precisa aprender mais. Por que não tenta novamente?';
+    } else  if(score > 7) {
+      return 'Muito bem! Mas você ainda pode melhorar.';
+    } else {
+      return 'Parabéns! Você foi incrível.';
+    }
+  }
+
   return (
     <div id="gameover">
       <img src={WellDone} alt="Fim do Quiz" />
-      <h2>Parabéns {'{nome}'}!</h2>
+      <h2>{outputEndGame()}</h2>
       <p className={"pontuacao"}>{(quizState.score/quizState.questions.length)*100}% Score</p>
       <p className={"complete"}>Quiz finalizado! Sua conscientização é essencial na prevenção da dengue.</p>
       <p>
